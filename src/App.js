a488e0ddb8f400c59f0d7db48b2f63de051ca892
Markus Muinonen <markus.muinonen@gmail.com>
2020-12-28 21:24:37 +0200

    Use new fancy components and hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react'
+import CenteringContainer from "./components/CenteringContainer";
+import Header from "./components/Header";
+import NameList from "./components/NameList";
+import Search from "./components/Search";
+import useNames from "./hooks/useNames";
 
-
-function App() {
+const App = () => {
+  useNames()
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <CenteringContainer>
+      <Header />
+      <Search />
+      <NameList />
+    </CenteringContainer>
+  )
 }
 
 export default App;
