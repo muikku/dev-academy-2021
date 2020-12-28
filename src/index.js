a8d4d38842dae2797d4e9e442064b422472c5d3a
Markus Muinonen <markus.muinonen@gmail.com>
2020-12-28 21:04:10 +0200

    Use state provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { reducer } from './reducer';
+import { StateProvider } from './state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider 
+      reducer={reducer}
+      initialState={{ filter: '', sort: null }}
+    >
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
