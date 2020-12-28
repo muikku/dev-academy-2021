c7e9ad95de022472eaf52119a9274bb77c5c9309
Markus Muinonen <markus.muinonen@gmail.com>
2020-12-28 21:22:37 +0200

    Create app header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Typography variant="h3">
+      Name Application
+    </Typography>
+  )
+}
+
+export default Header
